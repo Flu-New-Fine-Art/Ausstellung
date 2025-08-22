9a2b644a72d9125615304b95e674d828d8e94703
Flu-New-Fine-Art <heiko.loewenstein@arcor.de>
2025-08-22 18:24:28 +0200

    Add files via upload

---
+++ werke.js
@@ -0,0 +1,22 @@
+window.WERKE = [
+  {
+    id: "resonanzkern",
+    werk: "Resonanzkern",
+    serie: "Was bleibt",
+    ausstellung: "Crossing Cultures",
+    ort: "Kühlhaus Berlin",
+    datum: "03.10.25 – 05.10.25",
+    audio: "Resonanzkern.mp3",
+    pdf: "Resonanzkern.pdf"
+  },
+  {
+    id: "lichtpfad",
+    werk: "Lichtpfad",
+    serie: "Grenzzeit",
+    ausstellung: "Im Ich",
+    ort: "Kunstverein Hattingen",
+    datum: "10.11.25 – 20.12.25",
+    audio: "Lichtpfad.mp3",
+    pdf: "Lichtpfad.pdf"
+  }
+];
